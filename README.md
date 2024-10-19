a4c20ecc2db4791755f9d6646b5e402960a393b0
Kristina Gibout <kristina.gibout@gmail.com>
2024-10-18 18:40:18 -0700

    updated js and css

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ This is the third project of the Software Engineering program at TripleTen. It w
 - Positioning
 - Flat BEM file structure
 - Grid
+- Forms
+- Java Script
 [https://github.com/kristina-gibout/se_project_aroundtheus]
 [https://kristina-gibout.github.io/se_project_aroundtheus/]
 [https://drive.google.com/file/d/102FGpNNryv5KI_1Bs4e4vjouoJUI7DtD/view?usp=drive_link]
---
+++ scripts/validate.js
@@ -0,0 +1,76 @@
+const config = {
+formSelector: ".modal__form",
+inputSelector: ".modal__input",
+submitButtonSelector: ".modal__button",
+inactiveButtonClass: "modal__button_disabled",
+inputErrorClass: "modal__input_type_error",
+errorClass: "modal__error_visible"
+}
+
+
+function showInputError(formEl, inputEl, {inputErrorClass, errorClass}){
+const errorMessageEl = formEl.querySelector(`#$(inputEl.id)-error`);
+inputEl.classList.add(inputErrorClass);
+errorMessageEl.textContent = inputEl.validationMessage
+errorMessageEl.classList.add(errorClass)
+}
+
+function hideInputError(formEl, inputEl, {inputErrorClass, errorClass}){
+    const errorMessageEl = formEl.querySelector(`#$(inputEl.id)-error`);
+    inputEl.classList.remove(inputErrorClass);
+    errorMessageEl.textContent = ""
+    errorMessageEl.classList.remove(errorClass)
+    }
+
+function checkInputValidity(formEl, inputEl, options){
+if(!inputEl.validity.valid){
+    showInputError(formEl, inputEl, options);
+}else{hideInputError(formEl, inputEl, options)}
+}
+
+function hasInvalidInput(inputList){
+    return !inputList.every((inputEl)=> inputEl.validity.valid)
+}
+
+function disableBtn(){
+    submitBtn.classList.add(inactiveButtonClass);
+    return submitBtn.disabled = true;
+}
+
+function enableBtn(){
+    submitBtn.classList.remove(inactiveButtonClass);
+    submitBtn.disabled = false;
+}
+
+function toggleButtonState(inputEls, submitBtn, {inactiveButtonClass}){
+if(hasInvalidInput){
+    return disableBtn()
+}
+    enableBtn();
+
+}
+function setEventListeners(formEl, options){
+const {inputSelector} = options
+const inputEls = [...formEl.querySelectorAll(inputSelector)]
+const submitBtn = formEl.querySelector(".modal__button")
+inputEls.forEach(inputEl => {
+inputEl.addEventListener("input", (e) => {
+checkInputValidity(formEl, inputEl, options);
+toggleButtonState(inputEls, submitBtn, options)
+})
+})
+
+}
+
+function enableValidation(options){
+const formEls = [...document.querySelectorAll(options.formSelector)]
+formEls.forEach((formEl) => {
+    formEl.addEventListener("submit",(e) => {
+e.preventDefault();
+setEventListeners(formEl, options);
+    })
+})
+}
+
+
+enableValidation(config);
